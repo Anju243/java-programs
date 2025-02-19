8c018c5ab3cf3de8c1bab3aeaf91ebde4e245d79
Anju243 <anjugit68@gmail.com>
2025-02-19 11:56:36 +0530

    Add files via upload

---
+++ Userinput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Userinput
+{
+public static void main(String[] args){
+Scanner obj=new Scanner(System.in);
+System.out.println("enter the first number: ");
+int num1 = obj.nextInt();
+system.out.println("enter the second number:");
+int num2 = obj.nextInt();
+int sum=num1+num2;
+System.out.println("the sum of two numbers is: "+sum);
+}
+}
